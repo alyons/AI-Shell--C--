5ab250a96323df7900345413a4bdb9feab6f52be
Alexander Lyons <alexander.n.lyons@gmail.com>
2013-08-23 12:21:17 -0400

    Modify to include license agreement

--- AI Shell/AIShellLibraryTest/ConditionTest.cs
+++ AI Shell/AIShellLibraryTest/ConditionTest.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/// Copyright Alexander Lyons 2013
+///
+/// This file is part of AI Shell.
+///
+/// AI Shell is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+/// 
+/// AI Shell is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// 
+/// You should have received a copy of the GNU General Public License
+/// along with AI Shell.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
