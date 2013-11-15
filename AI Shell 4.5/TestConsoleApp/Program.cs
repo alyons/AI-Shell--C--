2f717a45bd11cf78dcd2da5b3e887f454a6f6e08
Alexander Lyons <alexander.n.lyons@gmail.com>
2013-11-15 15:36:23 -0500

    Update build config.

---
+++ AI Shell 4.5/TestConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using AI_Shell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing...");
+            string testString = "8.0564";
+            Console.WriteLine("Is " + testString + " a number of some sort? " + AIShellMathHelper.IsNumeric(testString));
+            Console.WriteLine("Any key to quit...");
+            Console.Read();
+        }
+    }
+}
